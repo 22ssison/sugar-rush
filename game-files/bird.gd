562e78d265c5c50a3074a5d1c3bb5e5ddc0235bf
22ssison <22ssison@wakatipu.school.nz>
2025-05-20 09:19:14 +1200

    finished basic character jumping/gravity

---
+++ game-files/bird.gd
@@ -0,0 +1,39 @@
+extends CharacterBody2D
+
+class_name Bird
+
+@export var gravity = 900.0 # can change this
+@export var jump_force = -300
+@export var rotation_speed = 2
+
+@onready var animation_player = $AnimationPlayer
+
+var max_speed = 400
+
+func _ready():
+	velocity = Vector2.ZERO #describing how our character is moving, in this case: its not going to move.
+	
+func _physics_process(delta):
+	if Input.is_action_just_pressed("jump"):
+		jump()
+	
+	velocity.y += gravity * delta
+	
+	velocity.y = min(velocity.y, max_speed)
+	
+	move_and_collide(velocity * delta)
+
+	rotate_bird()
+  
+func jump():
+	velocity.y = jump_force
+	rotation = deg_to_rad(-30) 
+	
+	
+func rotate_bird():
+	#rotate downwards when falling
+	if velocity.y > 0 && rad_to_deg(rotation) < 90:
+		rotation += rotation_speed * deg_to_rad(1)
+	# Rotate upwards when rising
+	elif velocity.y < 0 && rad_to_deg(rotation) > -30:
+		rotation -= rotation_speed * deg_to_rad(1)
