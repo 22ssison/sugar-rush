31fa64ffa4bfefb83cf90bdc1647b037b988657a
22ssison <22ssison@wakatipu.school.nz>
2025-07-25 13:35:52 +1200

    Made a high score saver.

--- game-files/Scenes/GameManager.gd
+++ game-files/Scenes/GameManager.gd
@@ -12,12 +12,18 @@ extends Node
 # then implement it on randomize()
 
 var points = 0
+var total_points = 0
+var high_score = 0
 
 func _ready():
+	
 	bird.game_started.connect(on_game_started)
 	ground.bird_crashed.connect(end_game)
 	pipe_spawner.bird_crashed.connect(end_game)
 	pipe_spawner.point_scored.connect(on_point_scored)
+	
+	load_high_score()               # Load high score at start
+
 	   
 func on_game_started(): 
 	label.visible = false # description of the game at the start.
@@ -31,6 +37,24 @@ func end_game():
 	
 func on_point_scored():
 	points += 1
+	total_points += 1
+	ui.update_high_score(high_score)  # Update UI display
+	if high_score < total_points:
+		high_score = total_points
+		save_high_score()           # Save immediately on new high score
 	ui.update_points(points)
+	
+func save_high_score():
+	var config = ConfigFile.new()
+	config.set_value("scores", "high_score", high_score)
+	config.save("user://save_game.cfg")
+
+func load_high_score():
+	var config = ConfigFile.new()
+	var err = config.load("user://save_game.cfg")
+	if err == OK:
+		high_score = config.get_value("scores", "high_score", 0)
+	else:
+		high_score = 0
 	
 # implement the random function to spawn random background, birds, etc all together. 
--- game-files/Scenes/ui.gd
+++ game-files/Scenes/ui.gd
@@ -4,16 +4,20 @@ class_name UI
 
 @onready var points_label: Label = $MarginContainer/PointsLabel
 @onready var game_over_box: VBoxContainer = $MarginContainer/GameOverBox
+@onready var high_score_label: Label = $HighScoreLabel
 
 func _ready():
 	points_label.text = "%d" % 0
-
+	
 func update_points(points: int):
 	points_label.text = "%d" % points
+	
+func update_high_score(high_score: int):
+	high_score_label.text = "high score = %d" % high_score
 
 func on_game_over():
 	game_over_box.visible = true
 
-func _on_button_pressed():
-	get_tree().reload_current_scene()
+func _on_button_pressed(): 
+	get_tree().reload_current_scene() #reloads everything including high score label. 
  
